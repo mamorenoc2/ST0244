96955e7e11784ea6eef531352e6e3ea6791a56ee
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2019-08-14 12:46:45 -0500

    Added io.py and io.cams.

---
+++ asm/io.py
@@ -0,0 +1,9 @@
+import disassembler
+
+def main ():
+    name = input("Enter your name: ")
+    age  = int(input("Enter your age: "))
+    print(name + ", a year from now you will be" ,age + 1, "years old.")
+
+# main()
+disassembler.disassemble(main)
