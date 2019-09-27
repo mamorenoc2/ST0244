2d7c61517ae13277c8e23abd78ec28828726a9cf
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2019-09-27 15:42:45 -0500

    [ oop ] Cosmetic changes.

--- oop/polymorphism.cpp
+++ oop/polymorphism.cpp
@@ -1,19 +1,23 @@
+// Tested with g++ (Ubuntu 5.4.0-6ubuntu1~16.04.11) 5.4.0 20160609.
+
+// From https://catonmat.net/cpp-polymorphism
+
 #include <iostream>
 #include "polymorphism.h"
 
 void
 do_meowing (Felid &cat)
 {
-  cat.meow();
+  cat.meow ();
 }
 
 int main ()
 {
-  Cat cat;
-  Tiger tiger;
+  Cat    cat;
+  Tiger  tiger;
   Ocelot ocelot;
 
-  do_meowing(cat);
-  do_meowing(tiger);
-  do_meowing(ocelot);
+  do_meowing (cat);
+  do_meowing (tiger);
+  do_meowing (ocelot);
 }
--- oop/polymorphism.h
+++ oop/polymorphism.h
@@ -4,20 +4,20 @@
 
 class Felid {
 public:
- virtual void meow () = 0;
+   virtual void meow () = 0;
 };
 
 class Cat : public Felid {
 public:
- void meow () { std::cout << "Meowing like a regular cat! meow!\n"; }
+   void meow () { std::cout << "Meowing like a regular cat! meow!\n"; }
 };
 
 class Tiger : public Felid {
 public:
- void meow () { std::cout << "Meowing like a tiger! MREOWWW!\n"; }
+   void meow () { std::cout << "Meowing like a tiger! MREOWWW!\n"; }
 };
 
 class Ocelot : public Felid {
- public:
+public:
   void meow () { std::cout << "Meowing like an ocelot! mews!\n"; }
 };
