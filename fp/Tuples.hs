19e278115ed805498104f2e36c316b65eaec35f5
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2019-10-18 17:24:31 -0500

    [ fp ] Added tuples example.

---
+++ fp/Tuples.hs
@@ -0,0 +1,11 @@
+-- Tested with GHC 8.8.1.
+
+-- Concatenation of list using currying.
+appendC :: [a] -> [a] -> [a]
+appendC []       ys = ys
+appendC (x : xs) ys = x : appendC xs ys
+
+-- Concatenation of list using tuples.
+appendT :: ([a],[a]) -> [a]
+appendT ([], ys)       = ys
+appendT ((x : xs), ys) = x : appendT (xs,ys)
